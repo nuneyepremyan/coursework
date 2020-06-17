020621b7617ae4cecf41fb6a36e0469a7c09201e
Nune Yepremyan <40946483+nuneyepremyan@users.noreply.github.com>
2020-06-17 21:22:32 +0400

    Update graph1.cpp

--- graph1.cpp
+++ graph1.cpp
@@ -5,41 +5,8 @@
 int main()
 {
 
-	Graph g("C:/Users/nunda/OneDrive/Desktop/a");
-	/*
-	g.addVertex("V1");
-	g.addVertex("V2");
-	g.addVertex("V3");
-	g.addVertex("V4");
-	g.addVertex("V5");
-	g.addVertex("V6");
-	g.addEdge("V1", "V2");
-	g.addEdge("V1", "V3");
-	g.addEdge("V2", "V5");
-	g.addEdge("V3", "V4");
-	g.addEdge("V3", "V5");
-	*/
-
-	/*
-	g.addVertex("s");
-	g.addVertex("e");
-	g.addVertex("c");
-	g.addVertex("d");
-	g.addVertex("b");
-	g.addVertex("a");
-	g.addVertex("t");
 	
-	g.addEdge("s", "e");
-	g.addEdge("s", "c");
-	g.addEdge("e", "c");
-	g.addEdge("c", "d");
-	g.addEdge("c", "b");
-	g.addEdge("d", "a");
-	g.addEdge("b", "t");
-	g.addEdge("a", "t");
 
-	*/
-	/*
 	g.addVertex("s");
 	g.addVertex("a");
 	g.addVertex("b");
@@ -73,11 +40,6 @@ int main()
 	
 	
 
-
-
-	//std::cout <<"Is graph connected? "<< std::boolalpha << g.isConnected() << std::endl;
-
-	std::cout << g.locVertexCon("a", "b")<<std::endl;
 	std::cout << "Vertex connectivity is: " << g.vertexConnectivity() << std::endl;
 
 	
